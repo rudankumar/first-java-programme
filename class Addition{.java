af93b4c2abadb8b883e6028f36860840637a8eab
Rudan Kumar <103348851+rudankumar@users.noreply.github.com>
2022-04-10 10:38:59 +0530

    Add files via upload

---
+++ class Addition{.java
@@ -0,0 +1,8 @@
+class Addition{
+    public static void main(String args[]) {
+        int a = 20;
+        int b = 10;
+        int sum = (a+b);
+        System.out.print(sum);
+    }
+}
---
+++ class My2nd{.java
@@ -0,0 +1,7 @@
+class My2nd{
+    public static void main(String args[]) {
+       System.out.print("*\n");
+       System.out.print("**\n");
+       System.out.print("***");
+    }
+}
